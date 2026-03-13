8ca962247b40166c1ff2f2c521049b1c1c783398
buckieboi <80875112+buckieboi@users.noreply.github.com>
2026-03-13 10:45:19 -0500

    walking sphere player and walls with collision

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v) * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
+    }
+}
